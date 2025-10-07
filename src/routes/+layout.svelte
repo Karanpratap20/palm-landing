5a39075b80b7a5ef0546d2803b98b86ad2baaf76
Karanpratap20 <karanpratap2003@gmail.com>
2025-10-07 20:06:25 +0530

    revert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
 	import "../app.css";
-	// No need for $props() destructuring here; SvelteKit uses <slot /> for content
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<!-- Responsive layout wrapper -->
-<div class="max-w-full mx-auto px-4 overflow-x-hidden">
-	<slot />
-</div>
+{@render children?.()}
