9b00eaadd7599b1c467d3eda65abeb4af084a662
Karanpratap20 <karanpratap2003@gmail.com>
2025-10-08 12:58:07 +0530

    static commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  
+
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: '', // optional: if you have a subpath on Hostinger or GitHub Pages
+    }
   }
 };
 
